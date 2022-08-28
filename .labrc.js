888833b885ec04562fdc4c60d411b3189f92af6b
Nikhil John <nikhiljohn1010@gmail.com>
2022-08-28 19:40:19 +0530

    Updated lab config

--- .labrc.js
+++ .labrc.js
@@ -1,11 +1,11 @@
 module.exports = {
   assert: '@hapi/code',
   coverage: true,
-  threshold: 98,
+  threshold: 100,
   lint: true,
   paths: ["test"],
   leaks: false,
   progress: 2,
-  reporter: ['console', 'html'],
-  output: ['stdout', './coverage/index.html']
+  reporter: ['console', 'lcov'],
+  output: ['stdout', 'lcov.info']
 };
